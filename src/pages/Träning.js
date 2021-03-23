6b76fb30cc30c06d7cdf5fa3143e74895ed61d9c
Cedrik von Heiroth <cedrikvonheiroth@gmail.com>
2021-03-23 23:28:48 +0100

    made traning component and added text

--- src/pages/Träning.js
+++ src/pages/Träning.js
@@ -1,20 +1,13 @@
 import React from 'react'
+import Grupper from '../components/Grupper.js'
 
 function Träning() {
     return (
         <div>
             <h1>Träning på Golden Leaf</h1>
             <p>På Golden Leaf erbjuder vi träning för alla olika nivåer. Oavsett om din målsättning är att bli världsmästare eller om du vill komma i form och träna för skojs skull är du välkommen till Golden Leaf! På grund av rådande pandemi och begränsade platser vill vi dock att ni kontaktar oss före ni kommer ner!</p>
+            <Grupper />
 
-            <h3>Barngrupp 8 - 12 år</h3>
-            <p>Träningsgruppen för våra yngsta medlemmar! Barngruppen tränar varje söndag kl 11:00 - 12:00 och träningarna leds av våra fighters Moses och Sara. Dessa passen fokuserar på träningsglädje och rörelse samtidigt som barnen får lära sig de tekniska grunderna i thaiboxning. </p>
-
-            <h3>Nybörjargrupp</h3>
-            <p>Här krävs inga förkunskaper. Passar både för dig som vill lära dig riktig thaiboxning och dig som vill ha en rolig träningsform. Det enda du behöver är lämpliga träningskläder, vattenflaska och ett gott humör! På grund av rådande pandemi har vi inga lånehandskar, utan det krävs att du har med egna boxningshandskar. Nybörjargruppen leds av våra </p>
-
-            <h3>Mellangrupp</h3>
-
-            <h3>Tävlingsgrupp</h3>
 
             <h2>Priser</h2>
             <h2>Tider</h2>
