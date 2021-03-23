2ff76db34016985ffa17b9321b1361f977542fdb
Cedrik von Heiroth <cedrikvonheiroth@gmail.com>
2021-03-23 23:35:51 +0100

    created new empty files

--- src/pages/Träning.js
+++ src/pages/Träning.js
@@ -7,10 +7,6 @@ function Träning() {
             <h1>Träning på Golden Leaf</h1>
             <p>På Golden Leaf erbjuder vi träning för alla olika nivåer. Oavsett om din målsättning är att bli världsmästare eller om du vill komma i form och träna för skojs skull är du välkommen till Golden Leaf! På grund av rådande pandemi och begränsade platser vill vi dock att ni kontaktar oss före ni kommer ner!</p>
             <Grupper />
-
-
-            <h2>Priser</h2>
-            <h2>Tider</h2>
         </div>
     )
 }
