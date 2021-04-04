9e88e02bf42f4b448744fd1a55fe58b95efd343d
Cedrik <cedrikvonheiroth@gmail.com>
2021-04-04 14:52:47 +0200

    working on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import styles from '../css/Footer.module.css'
+import goldFacebookLogo from '../icons/facebook-gold.svg'
+import goldInstagramLogo from '../icons/instagram-gold.svg'
 
 function Footer() {
     return (
         <div className={styles.footer} >
-            <p>Facebook</p>
-            <p>Instagram</p>
-            <p>E-mail</p>
+            
+            
+
+
+            <div className={styles.linkWrapper}>
+                <a href='https://www.facebook.com/GoldenLeafMuayThai' target="_blank" rel="noreferrer"><div className={styles.facebookLogoWrapper} >
+                    <img src={goldFacebookLogo} alt='facebook' /> 
+                </div><span className={styles.spanLink}>Golden Leaf Muay Thai</span></a>
+            </div>
+
+            <div className={styles.linkWrapper}>
+                <a href='https://www.instagram.com/goldenleafmt' target="_blank" rel="noreferrer"><div className={styles.instagramLogoWrapper} >
+                    <img src={goldInstagramLogo} alt='instagram' />
+                </div><span className={styles.spanLink}>Goldenleafmt</span></a>
+            </div>
+
+
+            <p>golden.leaf.muay.thai@hotmail.com</p>
             <p>Nobelvägen 17C, 214 29 Malmö </p>
-            <p>Golden Leaf Muay Thai ©2021 </p>
+            <p className={styles.copyright}>Golden Leaf Muay Thai ©2021 </p>
         </div>
     )
 }
