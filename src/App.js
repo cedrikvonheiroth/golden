5efd08a9d297c1faed28b6533d35a891d60fdbac
Cedrik <cedrikvonheiroth@gmail.com>
2021-04-05 16:03:21 +0200

    footer always at bottom

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import Footer from './components/Footer';
 import { BrowserRouter, Route, Switch} from 'react-router-dom';
 
 
+
+
 function App() {
   return (
     <div className="App">
--- src/pages/Priser.js
+++ src/pages/Priser.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import styles from '../css/Priser.module.css'
 
 
 function Priser() {
     return (
-        <div>
+        <div className={styles.priser}>
             <Hero />
             <h1>Priser</h1>
         </div>
